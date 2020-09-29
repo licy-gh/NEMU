f8a0c6e32c1700bd3961181c82c310bf85ddcb62
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 19:01:49 +0800

    >  compile NEMU 3019209380 pigeon Linux pigeon 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:01:49 up  1:10,  1 user,  load average: 0.48, 0.45, 0.36 e8a258a945650d3e5c794634edbe47281d36c2

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -24,8 +24,8 @@ typedef union {
 	/* Do NOT change the order of the GPRs' definitions. */
 	struct{
 		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		swaddr_t eip;
 	};
-	swaddr_t eip;
 
 } CPU_state;
 
