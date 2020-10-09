614112a7f911985ab8fbc0dccfd3fdfdb0156715
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-09 23:33:41 +0800

    >  compile NEMU 3019209380 pigeon Linux pigeon 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:33:41 up  4:57,  1 user,  load average: 0.28, 0.28, 0.21 53fe629b74ed938e7d964e9dfa2f90916e0eece1

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include <stdlib.h>
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
@@ -82,6 +83,7 @@ typedef struct token {
 } Token;
 
 Token tokens[32];
+int priority[32];
 int nr_token;
 
 static bool make_token(char *e) {
@@ -111,15 +113,50 @@ static bool make_token(char *e) {
 						break;
 					default:
 						tokens[nr_token].type = rules[i].token_type;
+						// DECNUM, HEXNUM:	0
+						// ==, !=:			1
+						// &&, ||:			2
+						// +, -  :			3
+						// *, /  :			4
+						// !     :			5
+						// (, )  :			6
+						switch (tokens[nr_token].type){
+							case EQ:
+							case NEQ:
+						    	priority[nr_token] = 1;
+								break;
+							case DAND:
+							case DOR:
+							    priority[nr_token] = 2;
+								break;
+							case PLUS:
+							case SUB:
+							    priority[nr_token] = 3;
+								break;
+							case MULTI:
+							case DIV:
+								priority[nr_token] = 4;
+								break;
+							case DNOT:
+							    priority[nr_token] = 5;
+								break;
+							case LBRAKT:
+							case RBRAKT:
+							    priority[nr_token] = 6;
+							default:
+								priority[nr_token] = 0;
+								break;
+						}
 						if(rules[i].token_type == DECNUM || HEXNUM){
 							if(strlen(rules[i].regex) >= 32)
-							    break;
+							    Assert(0, "overflow!");
 							else{
 								int j;
 								for(j = 0; j < strlen(rules[i].regex); j++)
 									tokens[nr_token].str[j] = rules[i].regex[j];
 							}
 						}
+						nr_token++;
 				}
 
 				break;
@@ -135,6 +172,69 @@ static bool make_token(char *e) {
 	return true; 
 }
 
+bool check_parentheses(int left, int right){
+	if(tokens[left].type == LBRAKT || tokens[right].type == RBRAKT){
+		int i, l_cnt = 0, r_cnt = 0;
+		for(i = left + 1; i <= right - 1; i++){
+			if(tokens[i].type == LBRAKT)
+			    l_cnt++;
+			if(tokens[i].type == RBRAKT && l_cnt)
+			    r_cnt++;
+		}
+		return l_cnt == r_cnt;
+	}
+	return false;
+}
+
+int dominant_operator(int left, int right){
+    int i = left;
+	int min_priority = 114514, pos = left;
+	for(; i <= right; i++){
+		if(priority[i]){
+			if(tokens[i].type == LBRAKT){
+				int lb_cnt = 1, j;
+				for(j = i + 1; j <= right && lb_cnt; j++){
+					if(tokens[j].type == LBRAKT) lb_cnt++;
+					if(tokens[j].type == RBRAKT) lb_cnt--;
+				}
+				i = j - 1;
+			}
+			if(priority[i] <= min_priority){
+				min_priority = priority[i];
+				pos = i;
+			}
+		}
+	}
+	return pos;
+}
+
+uint32_t eval(int left, int right){
+	if(left > right){
+	    Assert(0, "left greater than right");
+	}
+	else if(left == right){
+		if(tokens[left].type == DECNUM || HEXNUM){
+			return atoi(tokens[left].str);
+		}
+	}
+	else if(check_parentheses(left, right)){
+		return eval(left + 1, right - 1);
+	}
+	else{
+		int op = dominant_operator(left, right);
+		uint32_t val1 = eval(left, op - 1);
+		uint32_t val2 = eval(op + 1, right);
+		switch (tokens[op].type){
+		case PLUS:	return val1 + val2;
+		case SUB:	return val1 - val2;
+		case MULTI:	return val1 * val2;
+		case DIV:	return val1 / val2;	
+		default:	assert(0);
+		}
+	}
+
+}
+
 uint32_t expr(char *e, bool *success) {
 	if(!make_token(e)) {
 		*success = false;
@@ -142,7 +242,7 @@ uint32_t expr(char *e, bool *success) {
 	}
 
 	/* TODO: Insert codes to evaluate the expression. */
-	panic("please implement me");
-	return 0;
+	*success = true;
+	return eval(0, nr_token);
 }
 
